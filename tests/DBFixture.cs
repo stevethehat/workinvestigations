a3a306666046ff285589c45c1146c725cb097736
Steve Lamb <steve.lamb@ibcos.co.uk>
2018-06-08 12:21:44 +0100

    added more tests & utility functions

--- tests/DBFixture.cs
+++ tests/DBFixture.cs
@@ -8,7 +8,8 @@ namespace Tests{
         public AppDb Db {get;set;}
 
         public DatabaseFixture(){
-            Db = new AppDb("server=localhost;Database=priceupdates;Uid=root;Pwd=bh49bb;SslMode=none");
+            //Db = new AppDb("server=localhost;Database=priceupdates;Uid=root;Pwd=bh49bb;SslMode=none");
+            Db = new AppDb("server = 192.168.56.4; Database = Price_converter; Uid = root; Pwd = tqrzon; SslMode = none");
         }
         public void Dispose(){
 
