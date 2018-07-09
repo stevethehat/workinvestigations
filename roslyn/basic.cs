22ea2a523eb6a27e410f8905411dd64c53ae3104
Steve Lamb <steve.lamb@ibcos.co.uk>
2018-07-09 16:04:40 +0100

    renamed roslyn

---
+++ roslyn/basic.cs
@@ -0,0 +1,20 @@
+namespace Tests{
+    /// <summary>
+    /// a test comment
+    /// a second line
+    /// </summary>
+    public class ThisIsTest1{
+        protected readonly string ReadOnlyPorperty;
+        public ThisIsTest1(){
+
+        }
+
+        /// <summary>
+        /// a test method comment
+        /// </summary>
+        /// <param name="testInt"></param>
+        public void PublicVoidMethod(int testInt){
+
+        }
+    }
+}
