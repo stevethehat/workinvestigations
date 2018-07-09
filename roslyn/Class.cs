6a4942e250b7a402b491e6bfd39f4aac3e693fb4
Steve Lamb <steve.lamb@ibcos.co.uk>
2018-07-09 16:04:40 +0100

    renamed roslyn

---
+++ roslyn/Class.cs
@@ -0,0 +1,98 @@
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+
+namespace Rosyln{
+    public class BaseInfo{
+        public string Name;
+        public string Comment;
+        protected SyntaxNode _node;
+        /*
+        public BaseInfo(SyntaxNode node){
+            Name = node.Identifier.Text;
+        }
+        */
+        protected void GetComment(){
+            StringBuilder comment = new StringBuilder();
+            SyntaxTriviaList trivia = _node.GetLeadingTrivia();
+            var commentNodes = _node.DescendantNodes().OfType<DocumentationCommentTriviaSyntax>();
+            foreach(var commentNode in trivia){
+                //comment.AppendLine(commentNode.GetText().ToString());
+                comment.AppendLine(commentNode.ToString());
+            }
+
+            Comment = comment.ToString();
+        }
+    }
+
+    public class MethodInfo: BaseInfo{
+        public MethodInfo(MethodDeclarationSyntax node){
+            _node = node;
+            Name = node.Identifier.Text;
+            GetComment();
+        }
+    }
+
+    public class FieldInfo: BaseInfo{
+        public FieldInfo(VariableDeclaratorSyntax node){
+            _node = node;
+            Name = node.Identifier.Text;
+            GetComment();
+        }
+    }
+
+    public class ClassInfo: BaseInfo{
+        protected readonly ClassDeclarationSyntax _classNode;
+        public ClassInfo(SyntaxNode classNode){
+            _classNode = (ClassDeclarationSyntax)classNode;
+            _node = classNode;
+            Name = _classNode.Identifier.Text;
+            GetComment();
+        }
+
+        public static List<ClassInfo> FindClasses(SyntaxNode root){
+            List<ClassInfo> result = new List<ClassInfo>();
+            foreach(ClassDeclarationSyntax classNode in root.DescendantNodes().OfType<ClassDeclarationSyntax>()){
+                result.Add(new ClassInfo(classNode));
+            }
+            return result;
+        }
+
+        public object Definition(){
+            var methods = Methods();
+            var fields = Fields();
+            var definition = new {
+                name = Name,
+                methods = methods,
+                fields = fields
+            };
+
+            return definition;
+        }
+
+        protected List<MethodInfo> Methods(){
+            List<MethodInfo> result = new List<MethodInfo>();
+            foreach(var method in _classNode.DescendantNodes().OfType<MethodDeclarationSyntax>()){
+                result.Add(new MethodInfo(method));
+            }
+
+            return result;
+        }
+
+        protected List<FieldInfo> Fields(){
+            List<FieldInfo> result = new List<FieldInfo>();
+            foreach(var fields in _classNode.DescendantNodes().OfType<FieldDeclarationSyntax>()){
+                foreach(var field in fields.Declaration.Variables){
+                    result.Add(new FieldInfo(field));
+                }
+            }
+
+            return result;
+        }
+    }
+}
+    
---
+++ roslyn/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Newtonsoft.Json;
+
+
+namespace Rosyln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //string[] files = Directory.GetFiles(".", "*.cs");
+            // "C:\\Users\\lambbste\\Documents\\Development\\ibcos\\priceupdates\\priceupdates"
+            string[] files = Directory.GetFiles("../../priceupdates/priceupdates", "*.cs", SearchOption.AllDirectories);
+            //string[] files = Directory.GetFiles("C:\\Users\\lambbste\\Documents\\Development\\priceupdates\\priceupdates", "*.cs", SearchOption.AllDirectories);
+            //string[] files = Directory.GetFiles("C:\\Users\\lambbste\\Documents\\Development\\workinvestigations\\rosyln", "*.cs", SearchOption.AllDirectories);
+
+            foreach (string fileName in files){
+                var code = new StreamReader(fileName).ReadToEnd();
+                var test = SyntaxFactory.ParseCompilationUnit(code);
+                if(test != null){
+                    var root = (CompilationUnitSyntax)test; 
+
+                    List<ClassInfo> classes = ClassInfo.FindClasses(root);
+                    foreach(ClassInfo classNode in classes){
+                        var definition = new {
+                            fileName = fileName,
+                            classDefinition = classNode.Definition()
+                        };
+
+                        Console.WriteLine(JsonConvert.SerializeObject(definition)); 
+                    }
+                }
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
