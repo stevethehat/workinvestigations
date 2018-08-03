72397bbd58d0e9402691ab2bf28d3d54e0b6a8ff
Steve Lamb <steve@zenlogic.co.uk>
2018-08-03 20:52:15 +0100

    joining things..

--- router-app/src/components/MoneyFlow.js
+++ router-app/src/components/MoneyFlow.js
@@ -25,8 +25,9 @@ class MoneyFlow{
 
     setupAreas(){
         const height = 80;
-        this.areas = {
-            "capital": new Box(this.canvas, { x: this.columns[2], y: 10, width: 300, height: height, lineColor: "black", lineWidth: 1, title: "Hello" }),
+        var areas = this.areas;
+        areas = {
+            "capital": new Box(this.canvas, { x: this.columns[2], y: 10, width: 300, height: height, lineColor: "black", lineWidth: 1, title: "Capital" }),
             "fixedAssets": new Box(this.canvas, { x: this.columns[2], y: 200, width: 300, height: height, lineColor: "black", lineWidth: 1, title: "Fixed Assets" }),
             "sales": new Box(this.canvas, { x: this.columns[2], y: 400, width: 300, height: height, lineColor: "black", lineWidth: 1, title: "Sales" }),
             "invoicing": new Box(this.canvas, { x: this.columns[1], y: 700, width: 300, height: height, lineColor: "black", lineWidth: 1, title: "Invoicing" }),
@@ -34,6 +35,10 @@ class MoneyFlow{
             "grossProfit": new Box(this.canvas, { x: this.columns[0], y: 500, width: 300, height: height, lineColor: "black", lineWidth: 1, title: "Gross Profit" }),
             "expenses": new Box(this.canvas, { x: this.columns[0], y: 400, width: 300, height: height, lineColor: "black", lineWidth: 1, title: "Expenses" }),
         }
+        areas["capital"].join(areas["fixedAssets"], "bl", "tl");
+        areas["fixedAssets"].join(areas["sales"], "bc", "tc");
+        areas["sales"].join(areas["invoicing"], "bl", "r");
+        
     }
 }
 
