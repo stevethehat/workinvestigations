73c72234a65b3318da87aa74d24fd6ee044c92aa
Steve Lamb <steve@zenlogic.co.uk>
2018-08-24 19:13:43 +0100

    tidy up commented out code..

--- DDF/Program.cs
+++ DDF/Program.cs
@@ -12,28 +12,12 @@ namespace DDF
     {
         static void Main(string[] args)
         {
-            ConvertDDF("prerec");
-            ConvertDDF("pmfrec");
-            ConvertDDF("pcdrec");
+            //ConvertDDF("prerec");
+            //ConvertDDF("pmfrec");
+            //ConvertDDF("pcdrec");
             ConvertDDF("pcgrec");
-            ConvertDDF("ctfrec");
-            ConvertDDF("cmfrec");
-            /*
-            string ddfType = "cmfrec";
-            DDFFile ddfFile = new DDFFile("structure", ddfType);
-            //DDFFile ddfFile = new DDFFile("template", "tbaseaddressline");
-            var structure = ddfFile.Parse();
-
-            string outputFile = Path.Combine("output", $"{ddfType}.json");
-            string json = JsonConvert.SerializeObject(structure, Formatting.Indented);
-            
-            Console.WriteLine(json);
-
-            if(File.Exists(outputFile)){
-                File.Delete(outputFile);
-            }
-            System.IO.File.WriteAllText(outputFile, json);
-            */
+            //ConvertDDF("ctfrec");
+            //ConvertDDF("cmfrec");
 
             Console.WriteLine("Hello World!");
         }
