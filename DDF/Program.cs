8500e74ac299bd555ff77cc4063e9e98aff1b19a
Steve Lamb <steve.lamb@ibcos.co.uk>
2018-09-07 17:24:46 +0100

    just the latest change

--- DDF/Program.cs
+++ DDF/Program.cs
@@ -12,28 +12,20 @@ namespace DDF
     {
         static void Main(string[] args)
         {
-            ConvertDDF("prerec");
-            ConvertDDF("pmfrec");
-            ConvertDDF("pcdrec");
-            ConvertDDF("pcgrec");
-            ConvertDDF("ctfrec");
-            ConvertDDF("cmfrec");
-            /*
-            string ddfType = "cmfrec";
-            DDFFile ddfFile = new DDFFile("structure", ddfType);
-            //DDFFile ddfFile = new DDFFile("template", "tbaseaddressline");
-            var structure = ddfFile.Parse();
-
-            string outputFile = Path.Combine("output", $"{ddfType}.json");
-            string json = JsonConvert.SerializeObject(structure, Formatting.Indented);
-            
-            Console.WriteLine(json);
-
-            if(File.Exists(outputFile)){
-                File.Delete(outputFile);
-            }
-            System.IO.File.WriteAllText(outputFile, json);
-            */
+            ConvertDDF("deprec");
+            //ConvertDDF("prerec");
+            //ConvertDDF("pmfrec");
+            //ConvertDDF("pcdrec");
+            //ConvertDDF("pcgrec");
+            //ConvertDDF("ctfrec");
+            //ConvertDDF("cmfrec");
+            //ConvertDDF("ivtrec");
+            //ConvertDDF("vatrec");
+            //ConvertDDF("pihrec");
+            //ConvertDDF("pilrec");
+            //ConvertDDF("pohrec");
+            //ConvertDDF("polrec");
+            //ConvertDDF("powrec");
 
             Console.WriteLine("Hello World!");
         }
