95cff07c7468bd5ecfc16f11e8836b01019278c0
Steve Lamb <steve.lamb@ibcos.co.uk>
2019-02-11 13:23:32 +0000

    Updated extension name

--- extensions/synddf/src/extension.ts
+++ extensions/synddf/src/extension.ts
@@ -8,23 +8,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "helloworld" is now active!');
+	console.log('Congratulations, your extension "synddf" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.synddf', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('WOW Hello World!');
-	});
-
-	let wanker = vscode.commands.registerCommand('extension.wanker', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('You are such a wanker!!!');
 	});
 
 	vscode.languages.registerHoverProvider('synddf', {
@@ -38,25 +31,25 @@ export function activate(context: vscode.ExtensionContext) {
 			message.appendMarkdown(`##Hover ${text} Content\n`);
 			message.appendCodeblock('javascript', `var a = 'b'`);
 
-			var result = new vscode.Hover(message, range);
+			const result = new vscode.Hover(message, range);
 			return result;
 		}
 	});
 
 	vscode.languages.registerDeclarationProvider('synddf', {
 		provideDeclaration(document, position, provider) {
-			const range 	= document.getWordRangeAtPosition(position);
-			const text 		= document.getText(range);
-			const file 		= new vscode.Location(vscode.Uri.file(`/Users/stevelamb/Development/ibcos/investigations/DDF/repository/template/${text}.DDF`), new vscode.Position(0, 1));
+			const range 	    = document.getWordRangeAtPosition(position);
+			const text 		    = document.getText(range);
+            const rootFolder    = vscode.workspace.getConfiguration('synddf');
+            const file 		    = new vscode.Location(vscode.Uri.file(`${rootFolder.get('repositoryRootFolder')}\\template\\${text}.DDF`), new vscode.Position(0, 1));
 
-			vscode.window.showInformationMessage(`we are gonna find a thing..,,${text} in ${document.uri}`);
+			vscode.window.showInformationMessage(`we are gonna find a thing..in ${text} in ${document.uri}`);
 
 			return file;
 		}
 	});
 
 	context.subscriptions.push(disposable);
-	context.subscriptions.push(wanker);
 }
 
 // this method is called when your extension is deactivated
