91e9a9dcbce9970bfa031eb0ab12bf3cf2ebde3b
Steve Lamb <steve@zenlogic.co.uk>
2019-06-21 12:46:26 +0100

    DblDebugger

--- DblDebug/DblDebug/CoreDebug.cs
+++ DblDebug/DblDebug/CoreDebug.cs
@@ -1,10 +1,82 @@
 ﻿using System;
+using System.Threading.Tasks;
+using PrimS.Telnet;
+
 namespace DblDebug
 {
-    public class CoreDebug
+    public class CoreDebug: IDisposable
     {
-        public CoreDebug()
+        private readonly Func<bool, string> _input;
+        private readonly string _host;
+        private readonly int _port;
+        private Client _client;
+
+        public CoreDebug(string host, int port)
         {
+            _host = host;
+            _port = port;
+            Connect();
         }
+
+        public bool Command(string command)
+        {
+            bool result = true;
+            if("q" == command)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        protected string SendCommand(string command)
+        {
+            Task result;
+            _client.WriteLine(command);
+
+            //result = await _client.G("DBG>");
+            return "";
+        }
+
+        protected void Connect()
+        {
+            _client = new Client(_host, _port, new System.Threading.CancellationToken()); 
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    _client.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~CoreDebug()
+        // {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
