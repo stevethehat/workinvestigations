7cab3a9e268d64500de5433859410f38ac899ceb
Steve Lamb <steve.lamb@ibcos.co.uk>
2019-06-21 13:59:52 +0100

    tcpClient Work

--- DblDebug/DblDebug/Program.cs
+++ DblDebug/DblDebug/Program.cs
@@ -8,17 +8,14 @@ namespace DblDebug
         public static void Main(string[] args)
         {
             App app = new App();
-            /*
-            CoreDebug debug = new CoreDebug("localhost", 1024);
+            CoreDebug debug = new CoreDebug("172.16.128.21", 1024);
 
-            string input = "";
+            string input = "se st ov";
             while (debug.Command(input))
             {
-                Console.Write("DBG>");
+                //Console.Write("DBG>");
                 input = Console.ReadLine();
-
             }
-            */
             Console.WriteLine("Done");
             Console.ReadKey();  
         }
