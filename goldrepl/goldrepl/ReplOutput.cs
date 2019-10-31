8bc6613e9f6a093c22d59ee4f612e74545fed56d
Steve Lamb <steve.lamb@ibcos.co.uk>
2019-10-31 16:33:55 +0000

    split class

---
+++ goldrepl/goldrepl/ReplOutput.cs
@@ -0,0 +1,43 @@
+﻿using IronPython.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldRepl
+{
+    public partial class Repl
+    {
+        public void Output(object obj)
+        {
+            Type variableType = (Type)obj.GetType();
+
+            if (variableType.Name == "PythonDictionary")
+            {
+                PythonDictionary dictionary = obj as PythonDictionary;
+                foreach (object key in dictionary.Keys)
+                {
+                    Console.WriteLine($"{key} = {dictionary[key]}");
+                }
+
+                return;
+            }
+
+            PropertyInfo[] propertyInfo = variableType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+
+            foreach (PropertyInfo property in propertyInfo)
+            {
+                try
+                {
+                    Console.WriteLine($"{property.Name} = {property.GetValue(obj)}");
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
+    }
+}
